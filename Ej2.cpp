a85b6825b64c337d6b812a9c822b80da4a018a5d
LeonelCledon <166616600+LeonelCledon@users.noreply.github.com>
2024-08-18 12:25:02 -0300

    Update Ej2.cpp

--- Ej2.cpp
+++ Ej2.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
-using namespace std;
+
+int n(int natural);
+void show(int period);
 
 int main() {
+    int num;
+    std::cout<<"Ingrese el numero que desea calcular su suma"<<std::endl;
+    std::cin>>num;
+    show(num);
+    std::cout<<n(num);
+}
 
-    cout << "Examen Final Prog 3" << endl;
+void show(int period){
+    for (int i=1;i<=period;i++){
+        if (i<period) {
+            std::cout<<i<<"+";
+        } else{
+            std::cout<<i<<"=";
+        }
+    }
+}
 
-    return 0;
+int n(int natural){
+    int su=0;
+    if (natural==0){
+        return 0;
+    }
+    else {
+        return su=natural+n(natural-1);
+    }
 }
