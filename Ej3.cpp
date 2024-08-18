a36defa167cc09078e650dd29e3ee0f31dcba375
LeonelCledon <166616600+LeonelCledon@users.noreply.github.com>
2024-08-18 12:48:30 -0300

    Update Ej3.cpp

--- Ej3.cpp
+++ Ej3.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
-using namespace std;
+
+void exchange(int n, int& ext);
 
 int main() {
+    int num, r=0;
+    std::cout<<"Ingrese el numero que desea invertir"<<std::endl;
+    std::cin>>num;
+    if (num<0){
+        std::cout<<"Error, no se permiten numeros negativos";
+        return 1;
+    } else{
+        exchange(num,r);
+        std::cout<<"El numero invertido es: "<<r;
+    }
+}
 
-    cout << "Examen Final Prog 3" << endl;
-
-    return 0;
+void exchange(int n, int& ext){
+    if (n==0){
+        return;
+    } else{
+       ext=ext*10+n%10;
+        exchange(n/10,ext);
+    }
 }
